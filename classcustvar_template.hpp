6546419a465ce2c41f5f3ccb7bb610777d224c52
emf09 <93343039+fuhgeiu@users.noreply.github.com>
2024-11-30 21:41:14 -0600

    update

---
+++ classcustvar_template.hpp
@@ -0,0 +1,173 @@
+#include <iostream>
+
+// notes, this library will contain class and members and templates that are not used in every project
+// this is my library i will use and expand in projects to learn
+
+/*
+ * contents
+ *
+ * class template for array like container, container (0)
+ *
+ *
+ *
+ * class for project
+ *
+ *
+ * class template to read stream buffer and store into container (0)
+ *
+ *
+ */
+
+// mem adress 7f = in stack on 64bit system
+
+
+namespace cstd {
+
+
+// fixed container
+template<typename T, int S>                  // <size of container, data type>
+class mydataT {
+    
+    T m_fixedcont[S];                           // stack array
+    size_t m_length = S;
+    
+public:
+    
+    mydataT() {};                            // constructor
+    
+    int size() const {return S;}             // returns size as an int
+    
+    T& operator[](int index) {return m_fixedcont[index % S ];}           // return refernce
+    const T& operator[](int index) const {return m_fixedcont[index % S];}           // for use if array is const
+    
+    T* arraypointer () { return m_fixedcont; }                                  // return pointer to array
+};
+
+
+
+// dynamic container
+template <typename T>
+class contdynamic {
+    
+    T* cd_data = nullptr;
+    
+    size_t  cd_size = 0;
+    size_t cd_capacity = 0;
+    
+private:
+    
+    void allocate (size_t newCapacity, size_t multiplier) {             // (to be allocated, multiplier)
+
+        T* newblock = static_cast<T*>(::operator new ((newCapacity+cd_capacity) * sizeof(T) * multiplier));
+
+        for (size_t i = 0; i < cd_size; i++) {newblock[i] = std::move(cd_data[i]);}         // move stuff to newblock
+
+        for (size_t i = 0; i < cd_capacity; i++) {cd_data[i].~T();}
+
+        operator delete(cd_data);                                                         // free current block of memory
+
+        cd_data = newblock;                                                 // assign current data* to new allocated block
+        cd_capacity = ((newCapacity+cd_capacity)*1.5);                      // change the size to new block size
+    }
+    
+    void allocate (size_t newCapacity) {                                    // (to be allocated)
+
+        T* newblock = static_cast<T*>(::operator new ((newCapacity+cd_capacity) * sizeof(T) * 1.5));
+
+        for (size_t i = 0; i < cd_size; i++) {(newblock[i]) = std::move(cd_data[i]);}
+
+        for (size_t i = 0; i < cd_capacity; i++) {cd_data[i].~T();}                        // destruct current block of obejcts
+
+        operator delete(cd_data);
+        
+        cd_data = newblock;
+        cd_capacity = ((newCapacity+cd_capacity)*1.5);
+    }
+    
+    void allocate () {
+
+        size_t newCapacity = 5;
+
+        T* newblock = static_cast<T*>(::operator new ((newCapacity+cd_capacity) * sizeof(T) * 1.5));
+
+        for (size_t i = 0; i < cd_size; i++) {(newblock[i]) = std::move(cd_data[i]);}
+
+        for (size_t i = 0; i < cd_capacity; i++) {cd_data[i].~T();}                        // destruct current block of obejcts
+
+        operator delete(cd_data);
+        
+        cd_data = newblock;
+        cd_capacity = (newCapacity*1.5);
+    }
+    
+    
+public:
+    
+    contdynamic() {allocate(2);}                   // default constructor allocate 2*1.5
+    
+    ~contdynamic() {                                        // destructor
+
+        for (size_t i = 0; i < cd_capacity; i++) {cd_data[i].~T();}                        // destruct current block of obejcts
+
+        operator delete(cd_data);
+    }
+    
+    void append(const T& element) {
+        
+        if (cd_size >= cd_capacity){                         // to increase available container space
+            
+            allocate(cd_size*3/2);
+        }
+        
+        cd_data[cd_size] = element;                         // copy data to apped onto container and assign
+        cd_size++;                                          // increment size
+    }
+    
+    void append(const T&& element) {                         // to mvoe data must be rvalue to use move()
+        
+        if (cd_size >= cd_capacity){                         // to increase available container space
+            
+            allocate(cd_size*3/2);
+        }
+        
+        cd_data[cd_size] = element;                         // copy data to apped onto container and assign
+        cd_size++;                                          // increment size
+    }
+    
+    void popback() { cd_data[cd_size-1].~T(); cd_size -= 1;}
+
+    const T& peek () const {return cd_data[cd_size -1];}
+    
+    void print () {for (size_t i = 0; i < cd_size; i++) std::cout << cd_data[i];}
+    
+    const size_t length() const {return cd_size;}                                   // return size of container
+    
+    const size_t size() const {return cd_size * sizeof(T);}
+    
+    const T& operator[](size_t index) const {return cd_data[index % cd_size];}   // return const ref to index
+    
+    T& operator[] (size_t index)  {return cd_data[index % cd_size];}   // return refrence to data at index
+    
+};
+
+}
+
+/*  things to add
+ 
+ add a stream operator overload
+ 
+ add exception handling
+ 
+ add peek
+ 
+ change allocate funciton, it dosnt shrink currently
+ 
+ 
+ 
+ 
+ 
+ add documentation
+ 
+ */
+
+
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <cassert>
-#include "simple_stack.hpp"
+#include "classcustvar_template.hpp"
 
 
 class Dish {
@@ -11,9 +11,19 @@ public:
         this->description = description;
     }
 
-    std::string get_description() {
+    const std::string& get_description() const {
         return this->description;
     }
+};
+
+
+template <typename C>
+class stack : public cstd::contdynamic<C> {
+
+public:
+
+
+
 };
 
 
@@ -21,25 +31,63 @@ int main () {
 
     // stack intiilize
 //    stack<Dish> mystack1;
+/*
+ *      !!!WARNING, USING OBJECT POINTERS WILL PRODUCE UNDEFINED BAHVAIOR!!!
+ *
+ *      only use user defiend and primitive types
+ *
+ */
 
-    contdynamic<Dish> vector1;
+    cstd::contdynamic<Dish> cont1;
+
+    Dish dish1("ugly");
+    Dish dish2("pretty");
+    Dish dish3("heavy");
+    Dish dish4("light");
+
+    cont1.append(dish1);
+    cont1.append(dish2);
+    cont1.append(dish3);
+    cont1.append(dish4);
+
+    assert(cont1[0].get_description() == "ugly");
+    assert(cont1[1].get_description() == "pretty");
+    assert(cont1[2].get_description() == "heavy");
+    assert(cont1[3].get_description() == "light");
+
+    cont1.popback();
+    cont1.popback();
 
 
+    // using derived class stack
 
+    stack<Dish> dishes;
 
-    // empty
+    dishes.append(dish1);
 
+    dishes.append(dish2);
 
+    dishes.append(dish3);
 
-    // to get size/number of elements
+    dishes.append(dish4);
 
-    // pushback
+    assert(dishes[0].get_description() == "ugly");
+    assert(dishes[1].get_description() == "pretty");
+    assert(dishes[2].get_description() == "heavy");
+    assert(dishes[3].get_description() == "light");
 
-    // popback
+    assert(dishes.peek().get_description() == "light");
 
-    // peek
+    dishes.popback();
+    dishes.popback();
 
+    // this will produce error, as we poped back
+//    assert(dishes[2].get_description() == "heavy");
+//    assert(dishes[3].get_description() == "light");
 
+    assert(dishes.length() == 2);        // we popped back so size(number of used elements in container) = 2
+
+    assert(dishes.peek().get_description() == "pretty");
 
 
 }
